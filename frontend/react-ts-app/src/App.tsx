85cf457538cafa5224080c3aea15aca71a3dd05e
Wai Leung Chan <chan-wai-leung@hotmail.com>
2025-03-05 11:27:56 +0100

    Implement counter

--- frontend/react-ts-app/src/App.tsx
+++ frontend/react-ts-app/src/App.tsx
@@ -1,10 +1,56 @@
-import './App.css'
+import './App.css';
+import { useReducer } from 'react';
 
-function App() {
-  return (
-    <>
-    </>
-  )
+interface State {
+  count: number;
+  step: number;
 }
 
-export default App
+type Action =
+  | { type: 'INCREMENT' }
+  | { type: 'DECREMENT' }
+  | { type: 'CHANGE_STEP'; payload: number };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + state.step };
+    case 'DECREMENT':
+      return { ...state, count: state.count - state.step };
+    case 'CHANGE_STEP':
+      return { ...state, step: action.payload };
+    default: {
+      const exhaustiveCheck: never = action;
+      throw new Error(`Unexpected action type: ${exhaustiveCheck}`);
+    };
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0, step: 1 });
+
+  return (
+    <>
+      <h1>Count: {state.count}</h1>
+      <div>
+        <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
+        <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
+      </div>
+      <div>
+        <label htmlFor="step">Step: {state.step}</label>
+      </div>
+      <div>
+        <input
+          id="step"
+          type="range"
+          min="1"
+          max="10"
+          value={state.step}
+          onChange={(e) => dispatch({ type: 'CHANGE_STEP', payload: Number(e.target.value) })}
+        />
+      </div>
+    </>
+  );
+};
+
+export default App;
